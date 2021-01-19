654753f09cfac4fa0bfd42093b943b12d21f4a50
RMN <rizaman007@gmail.com>
2021-01-19 09:08:07 +0700

    First Feature

---
+++ Contoh.java
@@ -0,0 +1,10 @@
+class Contoh {  
+	public static void main(String[] args){ 
+		System.out.println("Judul");  	
+		System.out.println("RMN: Aplikasi Java 2020");  
+		System.out.println("--- *** ---"); 
+		System.out.println("END"); 
+		System.out.println("User 2"); 
+		System.out.println("User 1"); 
+	}  
+}  
